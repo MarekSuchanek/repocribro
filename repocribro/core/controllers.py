6aa02ba65cc6871783470793904c18c5f20009ec
Marek Suchánek <suchama4@fit.cvut.cz>
2017-01-17 14:05:33 +0100

    Preparing layout of basic pages

--- repocribro/core/controllers.py
+++ repocribro/core/controllers.py
@@ -1,8 +1,41 @@
 import flask
 
-core = flask.Blueprint('core', __name__, url_prefix='/')
+core = flask.Blueprint('core', __name__, url_prefix='')
 
 
 @core.route('/')
 def index():
     return flask.render_template('core/index.html')
+
+
+@core.route('/search')
+@core.route('/search/<query>')
+def search(query=None):
+    # TODO: do query in DB and send results to view
+    return flask.render_template('core/search.html')
+
+
+@core.route('/user/<username>')
+def user(username):
+    # TODO: get user from DB and send to view or redirect if org
+    return flask.render_template('core/user.html', username=username)
+
+
+@core.route('/org/<username>')
+def org(username):
+    # TODO: get org from DB and send to view or redirect if user
+    return flask.render_template('core/org.html', username=username)
+
+
+@core.route('/repo/<username>')
+def repo_redir(username):
+    # TODO: if org then redirect to org instead
+    return flask.redirect(flask.url_for('user', username=username))
+
+
+@core.route('/repo/<username>/<reponame>')
+def repo(username, reponame):
+    # TODO: get user from DB and send to view or redirect if org
+    return flask.render_template('core/repo.html',
+                                 username=username,
+                                 reponame=reponame)
