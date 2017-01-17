61e4bed7779fc921ca65f6f3a0689fabadbbab2b
Marek Suchánek <suchama4@fit.cvut.cz>
2017-01-17 14:19:27 +0100

    Custom error pages

--- repocribro/repocribro.py
+++ repocribro/repocribro.py
@@ -10,6 +10,26 @@ app.register_blueprint(core)
 # TODO: load all extensions
 
 
+@app.errorhandler(403)
+def not_found(error):
+    return flask.render_template('error/403.html'), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return flask.render_template('error/404.html'), 404
+
+
+@app.errorhandler(410)
+def not_found(error):
+    return flask.render_template('error/410.html'), 410
+
+
+@app.errorhandler(500)
+def not_found(error):
+    return flask.render_template('error/500.html'), 500
+
+
 def start():
     # TODO: improve CLI for start
     # TODO: load config file(s)
