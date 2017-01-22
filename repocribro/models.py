47b89b7a0408ea47bc10f3ea1419c604002b297c
Marek Suchánek <suchama4@fit.cvut.cz>
2017-01-22 18:52:01 +0100

    Adding Push, Commit and Release models
    Models needed for webhooks and displaying details prepared, will need more functions in order to be fully operable.

--- repocribro/models.py
+++ repocribro/models.py
@@ -153,10 +153,12 @@ class Repository(db.Model):
     secret = db.Column(db.String(255))
     owner_id = db.Column(db.Integer, db.ForeignKey('RepositoryOwner.id'))
     owner = db.relationship('RepositoryOwner', back_populates='repositories')
+    pushes = db.relationship('Push', back_populates='repository')
+    releases = db.relationship('Release', back_populates='repository')
 
-    VISIBILITY_PUBLIC=0
-    VISIBILITY_PRIVATE=1
-    VISIBILITY_HIDDEN=2
+    VISIBILITY_PUBLIC = 0
+    VISIBILITY_PRIVATE = 1
+    VISIBILITY_HIDDEN = 2
 
     def __init__(self, github_id, fork_of, full_name, name, languages,
                  description, private, owner, visibility_type, secret):
@@ -167,12 +169,13 @@ class Repository(db.Model):
         self.languages = languages
         self.description = description
         self.private = private
+        self.owner = owner
         self.visibility_type = visibility_type
         self.secret = secret
 
     @staticmethod
     def create_from_dict(repo_dict, owner,
-                         visibility_type = 0, secret = None):
+                         visibility_type=0, secret=None):
         fork_of = None
         if 'parent' in repo_dict:
             fork_of = repo_dict['parent']['id']
@@ -201,3 +204,110 @@ class Repository(db.Model):
     def __repr__(self):
         return '<GH Repository {} ({})>'.format(self.full_name, self.github_id)
 
+
+class Push(db.Model):
+    """Push from GitHub"""
+    __tablename__ = 'Push'
+    id = db.Column(db.Integer, primary_key=True)
+    after = db.Column(db.Integer)
+    before = db.Column(db.Integer)
+    ref = db.Column(db.String(255))
+    timestamp = db.Column(db.DateTime())
+    pusher_name = db.Column(db.UnicodeText())
+    pusher_email = db.Column(db.String(255))
+    sender_login = db.Column(db.String(40))
+    sender_id = db.Column(db.Integer())
+    repository_id = db.Column(db.Integer, db.ForeignKey('Repository.id'))
+    repository = db.relationship('Repository', back_populates='pushes')
+    commits = db.relationship('Commit', back_populates='push')
+
+    def __init__(self, after, before, ref, timestamp, pusher_name,
+                 pusher_email, sender_login, sender_id, repository):
+        self.after = after
+        self.before = before
+        self.ref = ref
+        self.timestamp = timestamp
+        self.pusher_name = pusher_name
+        self.pusher_email = pusher_email
+        self.sender_login = sender_login
+        self.sender_id = sender_id
+        self.repository = repository
+
+    def __repr__(self):
+        return '<GH Push {}-{}>'.format(self.before[0:7], self.after[0:7])
+
+
+class Commit(db.Model):
+    """Commit from GitHub"""
+    __tablename__ = 'Commit'
+    id = db.Column(db.Integer, primary_key=True)
+    sha = db.Column(db.String(40))
+    tree_sha = db.Column(db.String(40))
+    message = db.Column(db.UnicodeText())
+    timestamp = db.Column(db.DateTime())
+    author_name = db.Column(db.UnicodeText())
+    author_email = db.Column(db.String(255))
+    author_login = db.Column(db.String(40))
+    committer_name = db.Column(db.UnicodeText())
+    committer_email = db.Column(db.String(255))
+    committer_login = db.Column(db.String(40))
+    push_id = db.Column(db.Integer, db.ForeignKey('Push.id'))
+    push = db.relationship('Push', back_populates='commits')
+
+    def __init__(self, sha, tree_sha, message, timestamp,
+                 author_name, author_email, author_login,
+                 committer_name, committer_email, committer_login, push):
+        self.sha = sha
+        self.tree_sha = tree_sha
+        self.message = message
+        self.timestamp = timestamp
+        self.author_name = author_name
+        self.author_email = author_email
+        self.author_login = author_login
+        self.committer_name = committer_name
+        self.committer_email = committer_email
+        self.committer_login = committer_login
+        self.push = push
+
+    def __repr__(self):
+        return '<GH Commit {}>'.format(self.sha[0:7])
+
+
+class Release(db.Model):
+    """Release from GitHub"""
+    __tablename__ = 'Release'
+    id = db.Column(db.Integer, primary_key=True)
+    github_id = db.Column(db.Integer())
+    tag_name = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime())
+    published_at = db.Column(db.DateTime())
+    prerelease = db.Column(db.Boolean())
+    draft = db.Column(db.Boolean())
+    name = db.Column(db.UnicodeText())
+    body = db.Column(db.UnicodeText())
+    author_login = db.Column(db.String(40))
+    author_id = db.Column(db.Integer())
+    sender_login = db.Column(db.String(40))
+    sender_id = db.Column(db.Integer())
+    repository_id = db.Column(db.Integer, db.ForeignKey('Repository.id'))
+    repository = db.relationship('Repository', back_populates='releases')
+
+    def __init__(self, github_id, tag_name, created_at, published_at,
+                 prerelease, draft, name, body, author_id, author_login,
+                 sender_login, sender_id, repository):
+        self.github_id = github_id
+        self.tag_name = tag_name
+        self.created_at = created_at
+        self.published_at = published_at
+        self.prerelease = prerelease
+        self.draft = draft
+        self.name = name
+        self.body = body
+        self.author_login = author_login
+        self.author_id = author_id
+        self.sender_login = sender_login
+        self.sender_id = sender_id
+        self.repository = repository
+
+    def __repr__(self):
+        return '<GH Commit {}>'.format(self.sha[0:7])
