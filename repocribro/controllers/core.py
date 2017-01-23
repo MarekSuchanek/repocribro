ab2368067efbffb47c153659521fb974dd01fb84
Marek Suchánek <suchama4@fit.cvut.cz>
2017-01-23 19:16:59 +0100

    Retrieving user, org and repo for public browsing
    Basic entities are now retrieved from DB to be publicly accessible in core views. Views will come with extensible tabs soon. There are also some considerations about errors if not found or not public.

--- repocribro/controllers/core.py
+++ repocribro/controllers/core.py
@@ -1,4 +1,5 @@
 import flask
+from ..models import User, Organization, Repository
 
 core = flask.Blueprint('core', __name__, url_prefix='')
 
@@ -15,27 +16,57 @@ def search(query=None):
     return flask.render_template('core/search.html')
 
 
-@core.route('/user/<username>')
-def user(username):
-    # TODO: get user from DB and send to view or redirect if org
-    return flask.render_template('core/user.html', username=username)
+@core.route('/user/<login>')
+def user_detail(login):
+    user = User.query.filter_by(login=login).first()
+    if user is None:
+        is_org = Organization.query.filter(
+            Organization.login == login
+        ).exists()
+        if is_org:
+            # TODO: implement 410 (user deleted/archived)
+            # TODO: user renaming
+            flask.abort(404)
+        flask.flash('Oy! You wanted to access user, but it\'s an organization.'
+                    'We redirected you but be careful next time!', 'notice')
+        return flask.redirect(flask.url_for('core.org', login=login))
+    # TODO: gather & prepare tabs & pass to template
+    return flask.render_template('core/user.html', user=user)
 
 
-@core.route('/org/<username>')
-def org(username):
-    # TODO: get org from DB and send to view or redirect if user
-    return flask.render_template('core/org.html', username=username)
+@core.route('/org/<login>')
+def org_detail(login):
+    org = Organization.query.filter_by(login=login).first()
+    if org is None:
+        is_user = User.query.filter_by(login=login).exists()
+        if is_user:
+            # TODO: implement 410 (org deleted/archived)
+            # TODO: org renaming
+            flask.abort(404)
+        flask.flash('Oy! You wanted to access organization, but it\'s  auser.'
+                    'We redirected you but be careful next time!', 'notice')
+    # TODO: gather & prepare tabs & pass to template
+    return flask.render_template('core/org.html', login=login)
 
 
-@core.route('/repo/<username>')
-def repo_redir(username):
-    # TODO: if org then redirect to org instead
-    return flask.redirect(flask.url_for('user', username=username))
+@core.route('/repo/<login>')
+def repo_redir(login):
+    flask.flash('Seriously?! You forget to specify repository name, didn\'t '
+                'you? We redirected you but be careful next time!', 'notice')
+    return flask.redirect(flask.url_for('user', login=login))
 
 
-@core.route('/repo/<username>/<reponame>')
-def repo(username, reponame):
-    # TODO: get user from DB and send to view or redirect if org
-    return flask.render_template('core/repo.html',
-                                 username=username,
-                                 reponame=reponame)
+@core.route('/repo/<login>/<reponame>')
+def repo_detail(login, reponame):
+    repo = Repository.query.filter_by(
+        full_name='{}/{}'.format(login, reponame),
+    ).first()
+    if not repo.is_public():
+        # TODO: 404 or 410 (if were public in the past)?
+        flask.abort(404)
+    if repo is None:
+        # TODO: implement 410 (repo deleted/archived)
+        # TODO: repository renaming
+        flask.abort(404)
+    # TODO: gather & prepare tabs & pass to template
+    return flask.render_template('core/repo.html', repo=repo)
