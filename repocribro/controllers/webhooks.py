60301ff23200ff34bf1536afbdb2d5db3a48c567
Marek Suchánek <suchama4@fit.cvut.cz>
2017-01-27 07:48:15 +0100

    Webhook dispatcher will also lookup the repo
    Controller/dispatcher for receiving GitHub webhook will also find the repository and if is not registered in the app (DB), it will end with 404. Otherwise is repo passed to event processors (they would most probably need it) and at the end DB changes are commited, so there is no need to commit in processors.

--- repocribro/controllers/webhooks.py
+++ repocribro/controllers/webhooks.py
@@ -1,22 +1,23 @@
 import flask
 from ..github import GitHubAPI
+from ..models import Repository, db
 
 webhooks = flask.Blueprint('webhooks', __name__, url_prefix='/webhook/github')
 
 # TODO: move webhooks logic somewhere else
 
 
-def gh_webhook_push(data, deliver_id):
+def gh_webhook_push(repo, data, deliver_id):
     # TODO: lookup repository and add push (commits)
     return ''
 
 
-def gh_webhook_release(data, deliver_id):
+def gh_webhook_release(repo, data, deliver_id):
     # TODO: lookup repository and add release
     return ''
 
 
-def gh_webhook_repository(data, deliver_id):
+def gh_webhook_repository(repo, data, deliver_id):
     # TODO: lookup repository and update it
     return ''
 
@@ -40,12 +41,15 @@ def gh_webhook():
 
     if not agent.startswith('GitHub-Hookshot/'):
         return flask.abort(404)
-    if data is None:
+    if data is None or 'repository' not in data:
         return flask.abort(400)
     if not GitHubAPI.webhook_verify_signature(data, signature):
         return flask.abort(404)
 
-    for event_processor in hooks.get(event, []):
-        event_processor(data=data, deliver_id=delivery_id)
+    repo = Repository.query.get_or_404(data['repository']['id'])
 
+    for event_processor in hooks.get(event, []):
+        event_processor(repo=repo, data=data, deliver_id=delivery_id)
+
+    db.session.commit()
     return ''
