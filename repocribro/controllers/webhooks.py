601ae26bfe55b64eadb7d0f04f50901cd811b701
Marek Suchánek <suchama4@fit.cvut.cz>
2017-01-27 07:34:15 +0100

    Webhook controller and dispatcher
    GitHub webhook for receiving events checks the validity of message (user-agent, signature, and if content in json is present) and then pass (dispatch) it to registered event processor. Registered processors will be extensible in the future.

--- repocribro/controllers/webhooks.py
+++ repocribro/controllers/webhooks.py
@@ -1,30 +1,51 @@
 import flask
+from ..github import GitHubAPI
 
 webhooks = flask.Blueprint('webhooks', __name__, url_prefix='/webhook/github')
-# TODO: secure webhooks: https://developer.github.com/webhooks/securing/
+
+# TODO: move webhooks logic somewhere else
 
 
-@webhooks.route('', methods=['POST'])
-def gh_webhook():
-    return ''
-
-
-@webhooks.route('/push', methods=['POST'])
-def gh_push():
-    data = flask.request.get_json()
+def gh_webhook_push(data, deliver_id):
     # TODO: lookup repository and add push (commits)
     return ''
 
 
-@webhooks.route('/repository', methods=['POST'])
-def gh_repository():
-    data = flask.request.get_json()
+def gh_webhook_release(data, deliver_id):
+    # TODO: lookup repository and add release
+    return ''
+
+
+def gh_webhook_repository(data, deliver_id):
     # TODO: lookup repository and update it
     return ''
 
 
-@webhooks.route('/release', methods=['POST'])
-def release():
+# TODO: extensible registered hooks
+hooks = {
+    'push': [gh_webhook_push],
+    'release': [gh_webhook_release],
+    'repository': [gh_webhook_repository],
+}
+
+
+@webhooks.route('', methods=['POST'])
+def gh_webhook():
+    headers = flask.request.headers
+    agent = headers.get('User-Agent', '')
+    signature = headers.get('X-Hub-Signature', '')
+    delivery_id = headers.get('X-GitHub-Delivery', '')
+    event = headers.get('X-GitHub-Event', '')
     data = flask.request.get_json()
-    # TODO: lookup repository and add release
+
+    if not agent.startswith('GitHub-Hookshot/'):
+        return flask.abort(404)
+    if data is None:
+        return flask.abort(400)
+    if not GitHubAPI.webhook_verify_signature(data, signature):
+        return flask.abort(404)
+
+    for event_processor in hooks.get(event, []):
+        event_processor(data=data, deliver_id=delivery_id)
+
     return ''
