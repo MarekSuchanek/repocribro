da3a54b7b67e6a49c1c2aa6cb6e40f5b4e2677cb
Marek Suchánek <suchama4@fit.cvut.cz>
2017-02-03 18:52:05 +0100

    Core controller getting content from extensions
    Core controllers get data from extensions so core views can be extended easily. Using dicts also allows overriding content set by previous extensions.

--- repocribro/controllers/core.py
+++ repocribro/controllers/core.py
@@ -3,7 +3,7 @@ import flask_login
 import flask_sqlalchemy
 import injector
 
-from ..extending.helpers import ViewTab, Badge
+from ..extending import ExtensionsMaster
 from ..models import User, Organization, Repository
 
 core = flask.Blueprint('core', __name__, url_prefix='')
@@ -17,48 +17,24 @@ def index():
 @core.route('/search/')
 @core.route('/search')
 @core.route('/search/<query>')
-@injector.inject(db=flask_sqlalchemy.SQLAlchemy)
-def search(db, query=''):
-    # TODO: more attrs
-    # TODO: limits, nonempty search?
-    users = User.fulltext_query(
-        query, db.session.query(User)
-    ).all()
-    orgs = Organization.fulltext_query(
-        query, db.session.query(Organization)
-    ).all()
-    repos = Repository.fulltext_query(
-        query, db.session.query(Repository)
-    ).all()
-
-    # TODO: gather & prepare tabs & pass to template
-    tabs = [
-        ViewTab(
-            'repositories', 'Repositories', 0,
-            flask.render_template('core/search/repos_tab.html', repos=repos),
-            octicon='repo', badge=Badge(len(repos))
-        ),
-        ViewTab(
-            'users', 'Users', 1,
-            flask.render_template('core/search/users_tab.html', users=users),
-            octicon='person', badge=Badge(len(users))
-        ),
-        ViewTab(
-            'orgs', 'Organizations', 2,
-            flask.render_template('core/search/orgs_tab.html', orgs=orgs),
-            octicon='organization', badge=Badge(len(orgs))
-        ),
-    ]
+@injector.inject(ext_master=ExtensionsMaster)
+def search(ext_master, query=''):
+    # TODO: more attrs, limits & pages
+    tabs = {}
+    if query != '':
+        ext_master.call('view_core_search_tabs',
+                        query=query, tabs_dict=tabs)
 
     return flask.render_template(
-        'core/search.html', query=query, tabs=tabs,
+        'core/search.html', query=query, tabs=tabs.values(),
         active_tab=flask.request.args.get('tab', 'repositories')
     )
 
 
 @core.route('/user/<login>')
-@injector.inject(db=flask_sqlalchemy.SQLAlchemy)
-def user_detail(db, login):
+@injector.inject(db=flask_sqlalchemy.SQLAlchemy,
+                 ext_master=ExtensionsMaster)
+def user_detail(db, ext_master, login):
     user = db.session.query(User).filter_by(login=login).first()
     if user is None:
         is_org = db.session.query(Organization).filter(
@@ -72,31 +48,20 @@ def user_detail(db, login):
                     'We redirected you but be careful next time!', 'notice')
         return flask.redirect(flask.url_for('core.org', login=login))
 
-    # TODO: gather & prepare tabs & pass to template
-    tabs = [
-        ViewTab(
-            'details', 'Details', 0,
-            flask.render_template('core/user/details_tab.html', user=user),
-            octicon='person'
-        ),
-        ViewTab(
-            'repositories', 'Repositories', 1,
-            flask.render_template(
-                'core/repo_owner/repositories_tab.html', owner=user
-            ),
-            octicon='repo', badge=Badge(len(user.repositories))
-        ),
-    ]
+    tabs = {}
+    ext_master.call('view_core_user_detail_tabs',
+                    user=user, tabs_dict=tabs)
 
     return flask.render_template(
-        'core/user.html', user=user, tabs=tabs,
+        'core/user.html', user=user, tabs=tabs.values(),
         active_tab=flask.request.args.get('tab', 'details')
     )
 
 
 @core.route('/org/<login>')
-@injector.inject(db=flask_sqlalchemy.SQLAlchemy)
-def org_detail(db, login):
+@injector.inject(db=flask_sqlalchemy.SQLAlchemy,
+                 ext_master=ExtensionsMaster)
+def org_detail(db, ext_master, login):
     org = db.session.query(User).filter_by(login=login).first()
     if org is None:
         is_user = db.session.query(User).filter_by(login=login).exists()
@@ -107,24 +72,12 @@ def org_detail(db, login):
         flask.flash('Oy! You wanted to access organization, but it\'s  auser.'
                     'We redirected you but be careful next time!', 'notice')
 
-    # TODO: gather & prepare tabs & pass to template
-    tabs = [
-        ViewTab(
-            'details', 'Details', 0,
-            flask.render_template('core/org/details_tab.html', org=org),
-            octicon='organization'
-        ),
-        ViewTab(
-            'repositories', 'Repositories', 1,
-            flask.render_template(
-                'core/repo_owner/repositories_tab.html', owner=org
-            ),
-            octicon='repo', badge=Badge(len(org.repositories))
-        ),
-    ]
+    tabs = {}
+    ext_master.call('view_core_org_detail_tabs',
+                    org=org, tabs_dict=tabs)
 
     return flask.render_template(
-        'core/org.html', org=org, tabs=tabs,
+        'core/org.html', org=org, tabs=tabs.values(),
         active_tab=flask.request.args.get('tab', 'details')
     )
 
@@ -133,15 +86,11 @@ def org_detail(db, login):
 def repo_redir(login):
     flask.flash('Seriously?! You forget to specify repository name, didn\'t '
                 'you? We redirected you but be careful next time!', 'notice')
+    print('WTF')
     return flask.redirect(flask.url_for('core.user_detail', login=login))
 
 
-@core.route('/repo/<login>/<reponame>')
-@injector.inject(db=flask_sqlalchemy.SQLAlchemy)
-def repo_detail(db, login, reponame):
-    repo = db.session.query(Repository).filter_by(
-        full_name='{}/{}'.format(login, reponame),
-    ).first()
+def repo_detail_common(db, ext_master, repo):
     if repo is None:
         # TODO: implement 410 (repo deleted/archived)
         # TODO: repository renaming
@@ -150,64 +99,29 @@ def repo_detail(db, login, reponame):
         # TODO: 404 or 410 (if were public in the past)?
         flask.abort(404)
 
-    # TODO: gather & prepare tabs
-    tabs = [
-        ViewTab(
-            'details', 'Details', 0,
-            flask.render_template('core/repo/details_tab.html', repo=repo),
-            octicon='repo'
-        ),
-        ViewTab(
-            'releases', 'Releases', 1,
-            flask.render_template('core/repo/releases_tab.html', repo=repo),
-            octicon='tag', badge=Badge(len(repo.releases))
-        ),
-        ViewTab(
-            'updates', 'Updates', 2,
-            flask.render_template('core/repo/updates_tab.html', repo=repo),
-            octicon='git-commit', badge=Badge(len(repo.pushes))
-        ),
-    ]
+    tabs = {}
+    ext_master.call('view_core_repo_detail_tabs',
+                    repo=repo, tabs_dict=tabs)
 
     return flask.render_template(
-        'core/repo.html', repo=repo, tabs=tabs,
+        'core/repo.html', repo=repo, tabs=tabs.values(),
         active_tab=flask.request.args.get('tab', 'details')
     )
 
 
-# TODO: DRY (similar to repo_detail)
+@core.route('/repo/<login>/<reponame>')
+@injector.inject(db=flask_sqlalchemy.SQLAlchemy,
+                 ext_master=ExtensionsMaster)
+def repo_detail(db, ext_master, login, reponame):
+    repo = db.session.query(Repository).filter_by(
+        full_name='{}/{}'.format(login, reponame),
+    ).first()
+    return repo_detail_common(db, ext_master, repo)
+
+
 @core.route('/hidden-repo/<secret>')
-@injector.inject(db=flask_sqlalchemy.SQLAlchemy)
-def repo_detail_hidden(db, secret):
+@injector.inject(db=flask_sqlalchemy.SQLAlchemy,
+                 ext_master=ExtensionsMaster)
+def repo_detail_hidden(db, ext_master, secret):
     repo = db.session.query(Repository).filter_by(secret=secret).first()
-    if repo is None:
-        # TODO: implement 410 (repo deleted/archived)
-        # TODO: repository renaming
-        flask.abort(404)
-    if not repo.is_hidden:
-        # TODO: 404 or 410 (if were hidden in the past)?
-        flask.abort(404)
-
-    # TODO: gather & prepare tabs
-    tabs = [
-        ViewTab(
-            'details', 'Details', 0,
-            flask.render_template('core/repo/details_tab.html', repo=repo),
-            octicon='repo'
-        ),
-        ViewTab(
-            'releases', 'Releases', 1,
-            flask.render_template('core/repo/releases_tab.html', repo=repo),
-            octicon='tag'
-        ),
-        ViewTab(
-            'updates', 'Updates', 2,
-            flask.render_template('core/repo/updates_tab.html', repo=repo),
-            octicon='git-commit'
-        ),
-    ]
-
-    return flask.render_template(
-        'core/repo.html', repo=repo, tabs=tabs,
-        active_tab=flask.request.args.get('tab', 'details')
-    )
+    return repo_detail_common(db, ext_master, repo)
--- repocribro/ext_core.py
+++ repocribro/ext_core.py
@@ -1,7 +1,9 @@
-from .extending import Extension
-from .extending.helpers import ExtensionView
+import flask
 import flask_bower
 import flask_migrate
+
+from .extending import Extension
+from .extending.helpers import ViewTab, Badge
 
 
 class CoreExtension(Extension):
@@ -40,6 +42,91 @@ class CoreExtension(Extension):
         # flask_restless is not compatible with flask_injector!
         from .api import create_api
         api_manager = create_api(self.app, self.db)
+
+    def view_core_search_tabs(self, *args, **kwargs):
+        query = kwargs.get('query', '')
+        tabs_dict = kwargs.get('tabs_dict')
+
+        from .models import User, Organization, Repository
+        users = User.fulltext_query(
+            query, self.db.session.query(User)
+        ).all()
+        orgs = Organization.fulltext_query(
+            query, self.db.session.query(Organization)
+        ).all()
+        repos = Repository.fulltext_query(
+            query, self.db.session.query(Repository)
+        ).all()
+
+        tabs_dict['repositories'] = ViewTab(
+            'repositories', 'Repositories', 0,
+            flask.render_template('core/search/repos_tab.html', repos=repos),
+            octicon='repo', badge=Badge(len(repos))
+        )
+        tabs_dict['users'] = ViewTab(
+            'users', 'Users', 1,
+            flask.render_template('core/search/users_tab.html', users=users),
+            octicon='person', badge=Badge(len(users))
+        )
+        tabs_dict['orgs'] = ViewTab(
+            'orgs', 'Organizations', 2,
+            flask.render_template('core/search/orgs_tab.html', orgs=orgs),
+            octicon='organization', badge=Badge(len(orgs))
+        )
+
+    def view_core_user_detail_tabs(self, *args, **kwargs):
+        user = kwargs.get('user')
+        tabs_dict = kwargs.get('tabs_dict')
+
+        tabs_dict['details'] = ViewTab(
+            'details', 'Details', 0,
+            flask.render_template('core/user/details_tab.html', user=user),
+            octicon='person'
+        )
+        tabs_dict['repositories'] = ViewTab(
+            'repositories', 'Repositories', 1,
+            flask.render_template(
+                'core/repo_owner/repositories_tab.html', owner=user
+            ),
+            octicon='repo', badge=Badge(len(user.repositories))
+        )
+
+    def view_core_org_detail_tabs(self, *args, **kwargs):
+        org = kwargs.get('org')
+        tabs_dict = kwargs.get('tabs_dict')
+
+        tabs_dict['details'] = ViewTab(
+            'details', 'Details', 0,
+            flask.render_template('core/org/details_tab.html', org=org),
+            octicon='person'
+        )
+        tabs_dict['repositories'] = ViewTab(
+            'repositories', 'Repositories', 1,
+            flask.render_template(
+                'core/repo_owner/repositories_tab.html', owner=org
+            ),
+            octicon='repo', badge=Badge(len(org.repositories))
+        )
+
+    def view_core_repo_detail_tabs(self, *args, **kwargs):
+        repo = kwargs.get('repo')
+        tabs_dict = kwargs.get('tabs_dict')
+
+        tabs_dict['details'] = ViewTab(
+            'details', 'Details', 0,
+            flask.render_template('core/repo/details_tab.html', repo=repo),
+            octicon='repo'
+        )
+        tabs_dict['releases'] = ViewTab(
+            'releases', 'Releases', 1,
+            flask.render_template('core/repo/releases_tab.html', repo=repo),
+            octicon='tag', badge=Badge(len(repo.releases))
+        )
+        tabs_dict['updates'] = ViewTab(
+            'updates', 'Updates', 2,
+            flask.render_template('core/repo/updates_tab.html', repo=repo),
+            octicon='git-commit', badge=Badge(len(repo.pushes))
+        )
 
 
 def make_extension(*args, **kwargs):
