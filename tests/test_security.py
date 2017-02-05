2be3f2b683f87c23aa833b412f119b3bd94382dd
Marek Suchánek <suchama4@fit.cvut.cz>
2017-02-05 22:10:51 +0100

    Testing security login/logout
    Simple tests for calling login and logout procedures and checking if user is logged in with role and then logged out.

---
+++ tests/test_security.py
@@ -0,0 +1,38 @@
+import flask_login
+import pytest
+from werkzeug.exceptions import Forbidden
+
+
+from repocribro.security import login, logout, permissions
+from repocribro.models import UserAccount, Role
+
+
+def test_login_logout(app, empty_db, session):
+    with app.test_request_context('/'):
+        account = UserAccount()
+        account.id = 666
+        session.add(account)
+        session.commit()
+        login(account)
+        assert flask_login.current_user.id == 666
+        logout()
+        assert flask_login.current_user.is_anonymous
+
+
+def test_permission_admin(app, empty_db, session):
+    with app.test_request_context('/'):
+        @permissions.admin_role.require(403)
+        def test():
+            return 200
+
+        with pytest.raises(Forbidden):
+            assert test() == 200
+
+        role_admin = Role('admin', '')
+        account = UserAccount()
+        account.id = 666
+        account.roles.append(role_admin)
+        session.add(role_admin)
+        session.add(account)
+        session.commit()
+        login(account)
