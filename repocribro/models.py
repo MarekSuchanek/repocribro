c8546b2d17de0c839ec4e8a2dec3438c04371fcb
Marek Suchánek <suchama4@fit.cvut.cz>
2017-02-07 19:13:14 +0100

    Documented methods/classes of models via docstrings
    There are still attributes to be documented. During documentation see_repo was improved a bit for better look.

--- repocribro/models.py
+++ repocribro/models.py
@@ -7,12 +7,26 @@ from .database import db
 
 Base = flask_sqlalchemy.declarative_base()
 
+# TODO: docstrings for attributes
+
 
 class SearchableMixin:
+    """Mixin for models that support fulltext query"""
+
+    #: List of names of string/text attributes used for fulltext
     __searchable__ = []
 
     @classmethod
     def fulltext_query(cls, query_str, db_query):
+        """Add fulltext filter to the DB query
+
+        :param query_str: String to be queried
+        :type query_str: str
+        :param db_query: Database query object
+        :type db_query: ``sqlalchemy.orm.query.Query``
+        :return: Query with fulltext filter added
+        :rtype: ``sqlalchemy.orm.query.Query``
+        """
         query_str = '%{}%'.format(query_str)
         condition = sqlalchemy.or_(
             *[getattr(cls, col).like(query_str) for col in cls.__searchable__]
@@ -21,32 +35,89 @@ class SearchableMixin:
 
 
 class RoleMixin:
+    """Mixin for models representing roles"""
 
     def __eq__(self, other):
+        """Equality of roles is based on names
+
+        :param other: Role or its name to be compared with
+        :type other: ``repocribro.models.RoleMixin`` or str
+        :return: If names are equal
+        :rtype: bool
+        """
         return (self.name == other or
                 self.name == getattr(other, 'name', None))
 
     def __ne__(self, other):
+        """Inequality of roles is based on names
+
+        :param other: Role or its name to be compared with
+        :type other: ``repocribro.models.RoleMixin`` or str
+        :return: If names are not equal
+        :rtype: bool
+        """
         return not self.__eq__(other)
 
     def __hash__(self):
+        """Standard hashing via name
+
+        :return: Hash of role
+        :rtype: int
+        """
         return hash(self.name)
 
 
 class Anonymous(flask_login.AnonymousUserMixin):
+    """Anonymous (not logged) user representation"""
 
     @property
     def is_active(self):
+        """Check whether is user active
+
+        :return: False, anonymous is not active
+        :rtype: bool
+        """
         return False
 
     def has_role(self, role):
+        """Check whether has the role
+
+        :param role: Role to be checked
+        :type role: ``repocribro.models.RoleMixin``
+        :return: False, anonymous has no roles
+        :rtype: bool
+        """
         return False
 
     @property
     def rolenames(self):
+        """Get names of all roles of that user
+
+        :return: Empty list, anonymous has no roles
+        :rtype: list of str
+        """
         return []
 
+    def owns_repo(self, repo):
+        """Check if user owns the repository
+
+        :param repo: Repository which shoudl be tested
+        :type repo: ``repocribro.models.Repository``
+        :return: False, anonymous can not own repository
+        :rtype: bool
+        """
+        return False
+
     def sees_repo(self, repo):
+        """Check if user is allowed to see the repo
+
+        Anonymous can see only public repos
+
+        :param repo: Repository which user want to see
+        :type repo: ``repocribro.models.Repository``
+        :return: If user can see repo
+        :rtype: bool
+        """
         return repo.is_public
 
 
@@ -54,22 +125,56 @@ class UserMixin(flask_login.UserMixin):
 
     @property
     def is_active(self):
+        """Check whether is user active
+
+        :return: If user is active (can login)
+        :rtype: bool
+        """
         return self.active
 
     def has_role(self, role):
+        """Check whether has the role
+
+        :param role: Role to be checked
+        :type role: str
+        :return: If user has a role
+        :rtype: bool
+        """
         return role in (role.name for role in self.roles)
 
     @property
     def rolenames(self):
+        """Get names of all roles of that user
+
+        :return: List of names of roles of user
+        :rtype: list of str
+        """
         return [role.name for role in self.roles]
 
+    def owns_repo(self, repo):
+        """Check if user owns the repository
+
+        :param repo: Repository which shoudl be tested
+        :type repo: ``repocribro.models.Repository``
+        :return: If user owns repo
+        :rtype: bool
+        """
+        return repo.owner.github_id == self.github_user.github_id
+
     def sees_repo(self, repo):
-        return repo.is_public or \
-               repo.owner.github_id == self.github_user.github_id or \
-               self.has_role('admin')
+        """Check if user is allowed to see the repo
+
+        Must be admin or owner to see not public repo
+
+        :param repo: Repository which user want to see
+        :type repo: ``repocribro.models.Repository``
+        :return: If user can see repo
+        :rtype: bool
+        """
+        return repo.is_public or self.has_role('admin') or self.owns_repo(repo)
 
 
-# Many-to-Many
+#: Many-to-many relationship between user accounts and roles
 roles_users = db.Table(
     'RolesAccounts',
     sqlalchemy.Column('account_id',
@@ -101,11 +206,21 @@ class UserAccount(db.Model, UserMixin, SearchableMixin):
 
     @property
     def login(self):
+        """Get login name for user account from related GH user
+
+        :return: Login name
+        :rtype: str
+        """
         if self.github_user is None:
             return '<unknown>'
         return self.github_user.login
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<UserAccount (#{})>'.format(self.id)
 
 
@@ -125,6 +240,11 @@ class Role(db.Model, RoleMixin):
         self.description = description
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<Role {} (#{})>'.format(
             self.name, self.id
         )
@@ -185,19 +305,17 @@ class User(RepositoryOwner, SearchableMixin):
         self.hireable = hireable
         self.user_account = user_account
 
-    def update_from_dict(self, user_dict):
-        self.login = user_dict['login']
-        self.email = user_dict['email']
-        self.name = user_dict['name']
-        self.company = user_dict['company']
-        self.location = user_dict['location']
-        self.description = user_dict['bio']
-        self.blog_url = user_dict['blog']
-        self.avatar_url = user_dict['avatar_url']
-        self.hireable = user_dict['hireable']
-
     @staticmethod
     def create_from_dict(user_dict, user_account):
+        """Create new user from GitHub data and related user account
+
+        :param user_dict: GitHub data containing user
+        :type user_dict: dict
+        :param user_account: User account in app for GH user
+        :type user_account: ``repocribro.models.UserAccount``
+        :return: Created new user
+        :rtype: ``repocribro.models.User``
+        """
         return User(
             user_dict['id'],
             user_dict['login'],
@@ -212,7 +330,28 @@ class User(RepositoryOwner, SearchableMixin):
             user_account
         )
 
+    def update_from_dict(self, user_dict):
+        """Update user from GitHub data
+
+        :param user_dict: GitHub data containing user
+        :type user_dict: dict
+        """
+        self.login = user_dict['login']
+        self.email = user_dict['email']
+        self.name = user_dict['name']
+        self.company = user_dict['company']
+        self.location = user_dict['location']
+        self.description = user_dict['bio']
+        self.blog_url = user_dict['blog']
+        self.avatar_url = user_dict['avatar_url']
+        self.hireable = user_dict['hireable']
+
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<GH User {}, {} (#{})>'.format(
             self.login, self.github_id, self.id
         )
@@ -237,6 +376,13 @@ class Organization(RepositoryOwner, SearchableMixin):
 
     @staticmethod
     def create_from_dict(org_dict):
+        """Create new organization from GitHub data
+
+        :param org_dict: GitHub data containing organization
+        :type org_dict: dict
+        :return: Created new organization
+        :rtype: ``repocribro.models.Organization``
+        """
         return Organization(
             org_dict['id'],
             org_dict['login'],
@@ -250,6 +396,11 @@ class Organization(RepositoryOwner, SearchableMixin):
         )
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<GH Organization {}, {} (#{})>'.format(
             self.login, self.github_id, self.id
         )
@@ -291,8 +442,11 @@ class Repository(db.Model, SearchableMixin):
         cascade='all, delete-orphan'
     )
 
+    #: Constant representing public visibility within app
     VISIBILITY_PUBLIC = 0
+    #: Constant representing private visibility within app
     VISIBILITY_PRIVATE = 1
+    #: Constant representing hidden visibility within app
     VISIBILITY_HIDDEN = 2
 
     def __init__(self, github_id, fork_of, full_name, name, languages, url,
@@ -314,6 +468,23 @@ class Repository(db.Model, SearchableMixin):
     @staticmethod
     def create_from_dict(repo_dict, owner, webhook_id=None,
                          visibility_type=0, secret=None):
+        """Create new repository from GitHub and additional data
+
+        :param repo_dict: GitHub data containing repository
+        :type repo_dict: dict
+        :param owner: Owner of this repository
+        :type owner: ``repocribro.model.RepositoryOwner``
+        :param webhook_id: ID of registered webhook (if available)
+        :type webhook_id: int
+        :param visibility_type: Visibility type within app (default: public)
+        :type visibility_type: int
+        :param secret: Secret for hidden URL (if available)
+        :type secret: str
+        :return: Created new repository
+        :rtype: ``repocribro.models.Repository``
+
+        :todo: work with fork_of somehow
+        """
         fork_of = None
         if 'parent' in repo_dict:
             fork_of = repo_dict['parent']['id']
@@ -333,6 +504,11 @@ class Repository(db.Model, SearchableMixin):
         )
 
     def update_from_dict(self, repo_dict):
+        """Update repository attributes from GitHub data dict
+
+        :param repo_dict: GitHub data containing repository
+        :type repo_dict: dict
+        """
         self.full_name = repo_dict['full_name']
         self.name = repo_dict['name']
         self.languages = repo_dict['language']
@@ -342,13 +518,28 @@ class Repository(db.Model, SearchableMixin):
 
     @staticmethod
     def make_full_name(login, reponame):
+        """Create full name from owner login name and repository name
+
+        :param login: Owner login
+        :type login: str
+        :param reponame: Name of repository (without owner login)
+        :type reponame: str
+        :return: Full name of repository
+        :rtype: str
+        """
         return '{}/{}'.format(login, reponame)
 
     @property
     def owner_login(self):
+        """Get owner login from full name of repository
+
+        :return: Owner login
+        :rtype: str
+        """
         return self.full_name.split('/')[0]
 
     def generate_secret(self):
+        """Generate new unique secret code for repository"""
         import uuid
         import hashlib
         self.secret = ''.join([
@@ -358,17 +549,25 @@ class Repository(db.Model, SearchableMixin):
 
     @property
     def is_public(self):
+        """Check if repository is public within app"""
         return self.visibility_type == self.VISIBILITY_PUBLIC
 
     @property
     def is_private(self):
+        """Check if repository is private within app"""
         return self.visibility_type == self.VISIBILITY_PRIVATE
 
     @property
     def is_hidden(self):
+        """Check if repository is hidden within app"""
         return self.visibility_type == self.VISIBILITY_HIDDEN
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<GH Repository {}, {} (#{})>'.format(
             self.full_name, self.github_id, self.id
         )
@@ -415,7 +614,20 @@ class Push(db.Model, SearchableMixin):
         self.repository = repository
 
     @staticmethod
-    def create_from_dict(push_dict, sender_dict, repository):
+    def create_from_dict(push_dict, sender_dict, repo):
+        """Create new push from GitHub and additional data
+
+        This also creates commits of this push
+
+        :param push_dict: GitHub data containing push
+        :type push_dict: dict
+        :param sender_dict: GitHub data containing sender
+        :type sender_dict: dict
+        :param repo: Repository where this push belongs
+        :type repo: ``repocribro.models.Repository``
+        :return: Created new push
+        :rtype: ``repocribro.models.Push``
+        """
         push = Push(
             push_dict['after'],
             push_dict['before'],
@@ -426,13 +638,18 @@ class Push(db.Model, SearchableMixin):
             push_dict['pusher']['email'],
             sender_dict['login'],
             sender_dict['id'],
-            repository
+            repo
         )
         for commit_data in push_dict.get('commits', []):
             Commit.create_from_dict(commit_data, push)
         return push
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<GH Push {}-{} (#{})>'.format(
             self.before[0:7], self.after[0:7], self.id
         )
@@ -480,8 +697,17 @@ class Commit(db.Model, SearchableMixin):
 
     @staticmethod
     def create_from_dict(commit_dict, push):
-        # TODO: verify, there are some conflict in GitHub docs
-        #       (commits in webhook PushEvent vs Commits API)
+        """Create new commit from GitHub and additional data
+
+        :param commit_dict: GitHub data containing commit
+        :type commit_dict: dict
+        :param push: Push where this commit belongs
+        :type push: ``repocribro.models.Push``
+        :return: Created new commit
+        :rtype: ``repocribro.models.Commit``
+
+        :todo: verify, there are some conflict in GitHub docs
+        """
         return Commit(
             commit_dict['sha'],
             commit_dict['tree_sha'],
@@ -498,6 +724,11 @@ class Commit(db.Model, SearchableMixin):
         )
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<GH Commit {}, (#{})>'.format(
             self.sha, self.id
         )
@@ -550,6 +781,17 @@ class Release(db.Model, SearchableMixin):
 
     @staticmethod
     def create_from_dict(release_dict, sender_dict, repo):
+        """Create new release from GitHub and additional data
+
+        :param release_dict: GitHub data containing release
+        :type release_dict: dict
+        :param sender_dict: GitHub data containing sender
+        :type sender_dict: dict
+        :param repo: Repository where this release belongs
+        :type repo: ``repocribro.models.Repository``
+        :return: Created new release
+        :rtype: ``repocribro.models.Release``
+        """
         return Release(
             release_dict['id'],
             release_dict['tag_name'],
@@ -568,11 +810,17 @@ class Release(db.Model, SearchableMixin):
         )
 
     def __repr__(self):
+        """Standard string representation of DB object
+
+        :return: Unique string representation
+        :rtype: str
+        """
         return '<GH Release {}, {} (#{})>'.format(
             self.tag_name, self.github_id, self.id
         )
 
 
+#: List of all model classes for simple including
 all_models = [
     Commit,
     Push,
